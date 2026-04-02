bdfc14dcdf72b0b843c4e0192914d797b2fdf2e3
Faisdhani <dhanifais05@gmail.com>
2026-04-02 10:46:10 +0700

    Add files via upload

--- analyse_results.py
+++ analyse_results.py
@@ -1,252 +1,63 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  9 11:03:53 2019
-
-@author: isaaclera
-"""
-
-import collections
+from yafs.stats import Stats
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import pickle
-from scipy import stats
-import matplotlib.patheffects as pe
+import pandas as pd
 import os
-import networkx as nx
-import json
-import time
-import glob
 
-def set_box_color(bp, color):
-    plt.setp(bp['boxes'], color=color)
-    plt.setp(bp['whiskers'], color=color)
-    plt.setp(bp['caps'], color=color)
-    plt.setp(bp['medians'], color=color)
-    
-# =============================================================================
-# Boxplot matriz of each app - gtw/user
-# =============================================================================
-
-def drawBoxPlot_Both_USER_ax(app,dr,drILP,ax):
-     data_a=dr[dr.app==app].r.values
-     data_b=drILP[drILP.app==app].r.values
-     ticks = list(np.sort(dr[dr.app==app].user.unique()))
-     bpl = ax.boxplot(data_a, positions=np.array(range(len(data_a)))*2.0-0.4, sym='', widths=0.55,
-                      whiskerprops = dict(linewidth=2),
-                     boxprops = dict(linewidth=2),
-                      capprops = dict(linewidth=2),
-                     medianprops = dict(linewidth=2))
-     bpI = ax.boxplot(data_b, positions=np.array(range(len(data_b)))*2.0+0.4, sym='', widths=0.55,
-                         whiskerprops = dict(linewidth=2),
-                     boxprops = dict(linewidth=2),
-                      capprops = dict(linewidth=2),
-                     medianprops = dict(linewidth=2))
-     set_box_color(bpl, '#a6bddb')
-     set_box_color(bpI, '#e34a33')
-     ax.get_xaxis().set_ticks(range(0, len(ticks) * 2, 2))
-     ax.set_xticklabels(ticks)
-     ax.set_title("App:%i"%app)
-     ax.set_xlim(-2, len(ticks)*2)
-     ax.plot([], c='#a6bddb', label="Electre III",linewidth=3)
-     ax.plot([], c='#e34a33', label="Weighted aveg.",linewidth=3)
+size = 10000
+# THIS VALUE MUST BE UPDATED FROM YOUR EXECUTION (COPYPASTE)
+failures = [{'id': 398, 'module': False, 'time': 0}, {'id': 343, 'module': False, 'time': 100}, {'id': 48, 'module': False, 'time': 200}, {'id': 133, 'module': True, 'time': 300}, {'id': 288, 'module': False, 'time': 400}, {'id': 283, 'module': False, 'time': 500}, {'id': 374, 'module': False, 'time': 600}, {'id': 168, 'module': False, 'time': 700}, {'id': 331, 'module': False, 'time': 800}, {'id': 265, 'module': False, 'time': 900}, {'id': 119, 'module': False, 'time': 1000}, {'id': 233, 'module': False, 'time': 1100}, {'id': 353, 'module': False, 'time': 1200}, {'id': 337, 'module': True, 'time': 1300}, {'id': 199, 'module': False, 'time': 1400}, {'id': 232, 'module': False, 'time': 1500}, {'id': 13, 'module': False, 'time': 1600}, {'id': 95, 'module': False, 'time': 1700}, {'id': 317, 'module': False, 'time': 1800}, {'id': 163, 'module': False, 'time': 1900}, {'id': 67, 'module': False, 'time': 2000}, {'id': 279, 'module': True, 'time': 2100}, {'id': 267, 'module': False, 'time': 2200}, {'id': 147, 'module': False, 'time': 2300}, {'id': 174, 'module': False, 'time': 2400}, {'id': 202, 'module': False, 'time': 2500}, {'id': 310, 'module': False, 'time': 2600}, {'id': 206, 'module': False, 'time': 2700}, {'id': 153, 'module': False, 'time': 2800}, {'id': 193, 'module': False, 'time': 2900}, {'id': 17, 'module': False, 'time': 3000}, {'id': 281, 'module': False, 'time': 3100}, {'id': 393, 'module': False, 'time': 3200}, {'id': 236, 'module': False, 'time': 3300}, {'id': 65, 'module': True, 'time': 3400}, {'id': 197, 'module': False, 'time': 3500}, {'id': 392, 'module': False, 'time': 3600}, {'id': 307, 'module': False, 'time': 3700}, {'id': 213, 'module': False, 'time': 3800}, {'id': 345, 'module': False, 'time': 3900}, {'id': 201, 'module': False, 'time': 4000}, {'id': 380, 'module': False, 'time': 4100}, {'id': 227, 'module': False, 'time': 4200}, {'id': 177, 'module': False, 'time': 4300}, {'id': 216, 'module': True, 'time': 4400}, {'id': 382, 'module': False, 'time': 4500}, {'id': 2, 'module': False, 'time': 4600}, {'id': 312, 'module': False, 'time': 4700}, {'id': 326, 'module': False, 'time': 4800}, {'id': 354, 'module': False, 'time': 4900}, {'id': 292, 'module': False, 'time': 5000}, {'id': 321, 'module': False, 'time': 5100}, {'id': 218, 'module': False, 'time': 5200}, {'id': 159, 'module': False, 'time': 5300}, {'id': 22, 'module': False, 'time': 5400}, {'id': 348, 'module': False, 'time': 5500}, {'id': 222, 'module': False, 'time': 5600}, {'id': 75, 'module': False, 'time': 5700}, {'id': 191, 'module': False, 'time': 5800}, {'id': 183, 'module': False, 'time': 5900}, {'id': 131, 'module': False, 'time': 6000}, {'id': 127, 'module': False, 'time': 6100}, {'id': 210, 'module': False, 'time': 6200}, {'id': 247, 'module': False, 'time': 6300}, {'id': 243, 'module': False, 'time': 6400}, {'id': 171, 'module': False, 'time': 6500}, {'id': 84, 'module': False, 'time': 6600}, {'id': 64, 'module': False, 'time': 6700}, {'id': 231, 'module': False, 'time': 6800}, {'id': 344, 'module': False, 'time': 6900}, {'id': 318, 'module': False, 'time': 7000}, {'id': 325, 'module': False, 'time': 7100}, {'id': 264, 'module': False, 'time': 7200}, {'id': 31, 'module': False, 'time': 7300}, {'id': 6, 'module': False, 'time': 7400}, {'id': 5, 'module': False, 'time': 7500}, {'id': 299, 'module': True, 'time': 7600}, {'id': 42, 'module': False, 'time': 7700}, {'id': 248, 'module': False, 'time': 7800}, {'id': 126, 'module': False, 'time': 7900}, {'id': 28, 'module': False, 'time': 8000}, {'id': 61, 'module': True, 'time': 8100}, {'id': 205, 'module': False, 'time': 8200}, {'id': 66, 'module': False, 'time': 8300}, {'id': 104, 'module': False, 'time': 8400}, {'id': 284, 'module': False, 'time': 8500}, {'id': 121, 'module': False, 'time': 8600}, {'id': 181, 'module': False, 'time': 8700}, {'id': 145, 'module': False, 'time': 8800}, {'id': 160, 'module': False, 'time': 8900}, {'id': 73, 'module': False, 'time': 9000}, {'id': 41, 'module': False, 'time': 9100}, {'id': 364, 'module': False, 'time': 9200}, {'id': 200, 'module': True, 'time': 9300}, {'id': 82, 'module': False, 'time': 9400}, {'id': 245, 'module': False, 'time': 9500}, {'id': 330, 'module': False, 'time': 9600}, {'id': 9, 'module': False, 'time': 9700}, {'id': 56, 'module': False, 'time': 9800}, {'id': 290, 'module': False, 'time': 9900}]
+print("Number of nodes removed: ",len(failures))
 
 
-            
-def drawRequestOnTimeline(ticks,messagesbyTime,i=0):
-    
-    fig, ax = plt.subplots(figsize=(8.0,4.0))
-    ax.plot(ticks, messagesbyTime, '-',color='#756bb1',alpha=1.,linewidth=2)
-    
-    z = np.polyfit(ticks, messagesbyTime, 10)
-    p = np.poly1d(z)
-    
-    ax1 = ax.plot(ticks,p(ticks),":",color='#c1bcdc',linewidth=6,label="Total num. of requests",path_effects=[pe.Stroke(linewidth=8, foreground='purple'), pe.Normal()])
-    ax.set_xlabel("Simulation number: %i"%i, fontsize=12)
-    ax.set_ylabel("QoS satisfaction \n (num. of requests)", fontsize=12)
-    ax.tick_params(labelsize=10)
-    #ax.set_xlim(-20,2020)
-    #ax.set_ylim(0,120)
-    #plt.legend([ax1,ax2,ax3],['Total num. of requests','Partition','ILP'],loc="upper right",fontsize=18)
-    plt.legend(loc="lower left",fontsize=12)
-    plt.tight_layout()
-    #plt.savefig('TimeSerie_Requests-%i.pdf'%i, format='pdf', dpi=600)
+# Basic analysis
+time_loops = [["M.Action"]]
+s = Stats(defaultPath=os.path.join(os.getcwd(), "Results_%s_exp"%(size)))
+s.showResults2(size, time_loops=time_loops)
+
+# Visualization of latency time series:
+
+dfail = pd.DataFrame.from_dict(failures)
+dfail["date"]=dfail.time.astype('datetime64[s]')
+dfail.index = dfail.date
+dfail['module'] = dfail.module.astype(str)
+dfail = dfail.resample('10s').agg({"module": "-".join}) #OPTIONAL BE CAREFOUL: resampling -> agg events
+#dict(module=''))['module'].apply(list)
+
+df = s.df
+dfl = s.df_link
+
+#df[df.time_latency<0]
+#df["date"]=df.time_in.astype('timedelta64[s]')
+df["date"]=df.time_in.astype('datetime64[s]')
+df.index = df.date
+df = df.resample('10s').agg(dict(time_latency='mean'))
+
+timeLatency = df.time_latency.values
+ticks = range(len(timeLatency))
+
+ticksV = np.array(ticks)*10
 
 
-def getRbyApp(df,dtmp):
-    dr = pd.DataFrame(
-        columns=['app', 'user', 'r', 'time'])  # m - numero de mensajes enviados
+#OK
+        ### Latency Time and Allocation replicas
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(ticks, timeLatency, '-')
+ax1.set_ylim(timeLatency.min()-0.5,5)#timeLatency.max()+0.5)
+ax1.set_xlabel("Simulation time", fontsize=16)
+ax1.set_ylabel("Latency time", fontsize=16)
+for idx,item in enumerate(dfail.module):
+#    print(idx,item,len(item))
+ #   value = item == "True" ## OPTIONAL DESAAG. EVENTS
+    x = ticks[idx]
+    if len(item)==4:    
+        y = timeLatency[idx]+0.45
+#        ax1.plot(ticks[idx], timeLatency[idx]+2, marker='o', c="green")
+        ax1.arrow(x,4.7, 0, -0.35, head_width=15.0, head_length=0.15, fc='g', ec='g')
+    elif len(item)==5:
+        y = timeLatency[idx]+1
+        ax1.arrow(x,4.7, 0, -0.1, head_width=0.5, head_length=0.1, fc='black', ec='black')
+        #ax1.plot(x, y, marker='<', c="black")
+#plt.savefig('figure9b.pdf', format='pdf', dpi=600)        
 
-    ixloc = 0
-    for g in dtmp.keys():
-        ids = dtmp[g]
-        responses = []
-        times = []
-#        messages = []
-
-        # Firstly, it computes the mode in all the app,user transmissions
-#        for i in ids:
-#            messages.append(df[df.id == i].shape[0])  # number of messages send by the user
-#
-#        # Requests with a inferior number of messages are filtered
-#        msg = np.array(messages)
-#        # mode = stats.mode(msg).mode[0]
-#        mode = stats.mode(msg)[0][0]
-        mode = 1
-        print("-----"*10)
-        for i in ids:
-            dm = df[df.id == i]
-            print(dm)
-            if mode == dm.shape[0]:
-                r = dm['time_out'].max() - dm['time_emit'].min()
-                responses.append(r)
-                print(r)
-                times.append(dm['time_emit'].min())
-
-        resp = np.array(responses)
-
-        dr.loc[ixloc] = [g[0], g[1],resp, times]
-        ixloc += 1
-
-    return dr
-
-def prepare_results(pathSimple):
-    ### Latency
-    #ONLY ONE TIME
-   # if not os.path.exists(pathSimple+"dr_%s_%i.pkl"%("CQ",0)):
-        for case in ["CQ"]:
-            for it in range(nsimulations):
-                fCSV = "Results_%s_%i_%i.csv"%(case,simulationTime,it)
-                
-                df = pd.read_csv(pathSimple+fCSV)
-                dtmp = df[df["module.src"]=="None"].groupby(['app','TOPO.src'])['id'].apply(list)
-                print(dtmp)
-                dr= getRbyApp(df,dtmp)
-                
-#                dC = pd.DataFrame(index=np.array(timeC).astype('datetime64[s]'))
-#                dC["QTY"]=np.ones(len(timeC))
-#                dC = dC.resample('10s').agg(dict(QTY='sum'))
-#                messagesbyTime = dC.QTY.values
-                
-                dr.to_pickle(pathSimple+"dr_%s_%i.pkl"%(case,it))
-#                np.save(pathSimple+"messagesbyTime_%s_%i.npy"%(case,it),messagesbyTime)
-
-            
-                
-# =============================================================================
-# =============================================================================
-# #  GLOBAL VARIABLES
-# =============================================================================
-# =============================================================================
-timeSimulation=1000000
-
-pathEXP = "exp1/"
-nsimulations = 1
-#datestamp = time.strftime('%Y%m%d')
-datestamp = "20190131"
-
-pathSimple = glob.glob(pathEXP+"results_[0-9]*")[0]
-case ="CQ"
-
-resampleSize = 1000
-tickGeneration =  range(int(timeSimulation/resampleSize // 2.),timeSimulation//resampleSize,int(timeSimulation/resampleSize // 2.0 //10.0))
-    
-drs = {}
-color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
-                  '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
-                  '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
-                  
-for it in range(nsimulations):
-    fCSV = "Results_%s_%i_%i.csv"%(case,timeSimulation,it)    
-    df = pd.read_csv(os.path.join(pathSimple, fCSV))
-    dtmp = df[df["module.src"]=="None"].groupby(['app','TOPO.src'])['id'].apply(list)
-   
-    drs = {}
-    ticks = 0
-    maxValue= 0 
-    for g in dtmp.keys():
-        print(g)
-        ids = dtmp[g]
-
-        t = []
-        intime = []
-        for i in ids:
-            dm = df[df.id == i]
-            t.append(dm['time_reception'].max()- dm['time_emit'].min())
-            intime.append(dm['time_emit'].min())
-
-        dr = pd.DataFrame() 
-        dr["time"] = t
-        dr.index = np.array(intime).astype('datetime64[s]')
-        # BUGON: (2,12) gives 549 elems, while should 550
-        dr = dr.resample('1000s').agg(dict(time='mean'))[0:549]  
-        maxValue=max(maxValue,dr.time.max())
-        ticks = range(len(dr.time.values))
-        drs[(g[0],g[1])] = dr.time.values
-        print("-"*40)
-
-
-    fig, ax = plt.subplots(figsize=(12.0,4.0))
-    for i,k in enumerate(drs):
-        ax.plot(ticks, drs[k],'-',alpha=1.,linewidth=2,label="S%i on '%i'"%(k[0],k[1]), color=color_sequence[i])       
-
-    plt.text(tickGeneration[0]-110, maxValue-20, "Generation:", fontsize=12, color=color_sequence[-1])
-    for i,xc in enumerate(tickGeneration):
-        plt.axvline(x=xc,color=color_sequence[-1])
-        xsep=15
-        if i+1>=10: xsep+=10
-        plt.text(xc-xsep, maxValue-20, i+1, fontsize=14, color=color_sequence[-1])
-        
-    ax.set_xlabel("Simulation Time ( simulation number %i)"%it, fontsize=12)
-    ax.set_ylabel("Latency", fontsize=12)
-    ax.tick_params(labelsize=10)
-    ax.set_xlim(0,len(ticks))
-    #ax.set_ylim(0,120)
-    leg = ax.legend();
-    #plt.legend([ax1,ax2,ax3],['Total num. of requests','Partition','ILP'],loc="upper right",fontsize=18)
-    plt.legend(loc="upper left",fontsize=12)
-    plt.tight_layout()
-
-    #SUMMARIZATION TABLE   (an to latex) 
-    listG = ["case","base (msg)","base (mean)","base (std)"]
-    for i,xc in enumerate(tickGeneration):
-        listG.append("G%i (msg)"%(i+1))
-        listG.append("G%i (mean)"%(i+1))
-        listG.append("G%i (std)"%(i+1))
-          
-    dlat = pd.DataFrame(columns=listG)
-    ixloc = 0    
-    for i,k in enumerate(drs):
-
-        ds = pd.DataFrame() 
-        ds["time"] = drs[k]
-        msg,mean,std = [],[],[]
-
-        inter = (0, *tickGeneration, resampleSize)
-        
-        for gen in range(len(inter)-1):
-#            print(inter[gen],inter[gen+1])
-            msg.append(len(ds.time[inter[gen]:inter[gen+1]]))
-            mean.append(ds.time[inter[gen]:inter[gen+1]].mean())
-            std.append(ds.time[inter[gen]:inter[gen+1]].std())
-        
-        row = ["S%i on '%i'"%(k[0],k[1])]
-        for i in range((len(listG)-1)//3):
-            row.append(msg[i])
-            row.append(mean[i])
-            row.append(std[i])
-           
-        dlat.loc[ixloc] = row
-        ixloc +=1
-#    print(dlat.columns)
-#    print(len(dlat.columns))
-    print("MAIN METRICS")
-    print(dlat.iloc[:,[0,2,5,32]])
-    print("-"*30)
-    
-    with open(pathSimple+'mytable_%i.tex'%it,'w') as tf:
-       tf.write(dlat.to_latex(index=False))
-       
- 
